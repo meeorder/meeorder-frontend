22e04af866cd8d87fb4d677a67fb842318ea734d
karnse <suphawit.se@ku.th>
2023-07-23 18:00:46 +0700

    styled follow futter in figma

--- src/modules/menu/components/SimpleCategory.tsx
+++ src/modules/menu/components/SimpleCategory.tsx
@@ -1,6 +1,7 @@
 import { type CategoryProps } from "@/modules/menu/components/Category";
+import { PlusOutlined } from "@ant-design/icons";
 import styled from "@emotion/styled";
-import { Image, List } from "antd";
+import { Button, Col, Image, List } from "antd";
 import React from "react";
 
 const SimpleCategory: React.FC<CategoryProps> = ({ category, foods }) => {
@@ -17,11 +18,19 @@ const SimpleCategory: React.FC<CategoryProps> = ({ category, foods }) => {
       renderItem={(item) => (
         <List.Item key={item.id}
           extra={
-          <StyledImage
-            width={100}
-            alt="pic"
-            src={item.imagePath}
-          />}>
+            <Col>
+              <StyledImage
+                width={100}
+                alt="pic"
+                src={item.imagePath}
+              />
+              <StyledButton
+                type="primary"
+                shape="circle"
+                icon={<PlusOutlined/>}
+              />
+            </Col>
+          }>
           <List.Item.Meta
             title={item.name}
             description={`\$${item.price}`}
@@ -40,21 +49,48 @@ const StyledList = styled(List)`
     padding: 0px;
   }
   .ant-list-item {
-    padding: 8px;
+    padding: 0px;
+    display: flex;
+    min-height: 117px;
+    align-items: flex-start;
+    align-self: stretch;
+    border: 1px solid var(--neutral-4, #F0F0F0);
+    background: var(--neutral-1, #FFF);
   }
   .ant-list-item-meta {
-    padding-left: 24px;
+    display: flex;
+    width: 240px;
+    padding: 24px;
+    align-items: flex-start;
+    gap: 8px;
+  }
+  .ant-image {
+    right: 8px;
+    top: 8px;
+  }
+  .ant-image-mask {
+    display: flex;
+    align-items: flex-start;
+    gap: 10px;
+    position: absolute;
+    right: 8px;
+    top: 8px;
   }
 `
 const StyledImage = styled(Image)`
-    margin-top: 8px;
-    margin-bottom: 8px;
-    align-self: stretch;
-    width: 100%;
-    height: 100%;\
-    border-radius: 12px;
-    background:
-        linear-gradient(0deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.2) 100%),
-        url(<path-to-image>),
-        lightgray -2.26px -11.135px / 103.623% 134.454% no-repeat;
+  border-radius: 5.667px;
+  background: url(<path-to-image>), lightgray -2.864px -1.372px / 134.727% 165.318% no-repeat;
 `;
+const StyledButton = styled(Button)`
+  position: absolute;
+  right: 11px;
+  top: 73px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border: 1px solid var(--primary-6, #1890FF);
+  background: var(--primary-6, #1890FF);
+  /* drop-shadow/button-primary */
+  box-shadow: 0px 2px 0px 0px rgba(0, 0, 0, 0.04);
+`
