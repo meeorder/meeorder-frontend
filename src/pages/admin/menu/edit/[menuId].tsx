6c2cf180f195fb3053a8142f0657e6445d58259a
GolfGrab <surapus01@gmail.com>
2023-07-29 00:40:04 +0700

    Add admin Layout mock pages and AdminSideNav

---
+++ src/pages/admin/menu/edit/[menuId].tsx
@@ -0,0 +1,11 @@
+import AppLayout from "@/modules/AppLayout";
+
+const AdminEditMenu = () => {
+  return (
+    <AppLayout layoutType="admin" currentPageId="adminAddEditMenu">
+      AdminEditMenu
+    </AppLayout>
+  );
+};
+
+export default AdminEditMenu;
