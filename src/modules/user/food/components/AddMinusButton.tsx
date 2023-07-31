3213d6654c6e536a254f23114a806775a2236e5e
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-08-01 02:36:44 +0700

    Create Add/Mins button and SaveButton

---
+++ src/modules/user/food/components/AddMinusButton.tsx
@@ -0,0 +1,56 @@
+import { Text } from "@/modules/common/components/Typography";
+import styled from "@emotion/styled";
+import { Minus, Plus } from "@phosphor-icons/react";
+import { Button } from "antd";
+import React, { type Dispatch, type SetStateAction } from "react";
+
+type AddMinusButtonProps = {
+  count: number;
+  setCount: Dispatch<SetStateAction<number>>;
+};
+
+const AddMinusButton: React.FC<AddMinusButtonProps> = ({ count, setCount }) => {
+  return (
+    <ButtonAddMinusContainer>
+      <Button
+        shape="circle"
+        size="large"
+        icon={<Minus size={16} />}
+        disabled={count === 0}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          boxShadow: "0px 2px 0px 0px rgba(0, 0, 0, 0.04)",
+        }}
+        onClick={() => {
+          if (count > 0) {
+            setCount(count - 1);
+          }
+        }}
+      />
+      <Text>{count}</Text>
+      <Button
+        shape="circle"
+        size="large"
+        icon={<Plus size={16} />}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          boxShadow: "0px 2px 0px 0px rgba(0, 0, 0, 0.04)",
+        }}
+        onClick={() => setCount(count + 1)}
+      />
+    </ButtonAddMinusContainer>
+  );
+};
+
+export default AddMinusButton;
+
+const ButtonAddMinusContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 120px;
+  align-items: center;
+`;
---
+++ src/modules/user/food/components/SaveButton.tsx
@@ -0,0 +1,30 @@
+import { H5 } from "@/modules/common/components/Typography";
+import { Button } from "antd";
+import React from "react";
+
+type SaveButtonProps = {
+  count: number;
+};
+
+const SaveButton: React.FC<SaveButtonProps> = ({ count }) => {
+  return (
+    <>
+      <Button
+        type="primary"
+        size="large"
+        danger={count === 0}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <H5 style={{ color: "white" }}>
+          {count === 0 ? "Delete From My Cart" : "Save change $99.00"}
+        </H5>
+      </Button>
+    </>
+  );
+};
+
+export default SaveButton;
