0b0d7f15067504c69153e8c439f08ec61f6d375d
AdroitBit <yanothaic15@gmail.com>
2023-07-31 19:37:02 +0700

    mock organization

--- src/modules/admin/menu/AddOnFormSection.tsx
+++ src/modules/admin/menu/AddOnFormSection.tsx
@@ -1,66 +1,12 @@
 import { type MenuLayoutVariant } from "@/modules/admin/layout/AdminMenuLayout";
+import { addOnData, columns } from "@/modules/admin/mock/addon";
 import { H4 } from "@/modules/common/components/Typography";
 import styled from "@emotion/styled";
 import { Button, Card, Table } from "antd";
-import { type ColumnsType } from "antd/es/table";
 
 type AddOnFormSectionProps = {
   menuLayoutVariant: MenuLayoutVariant;
 };
-type AddOnDataType = {
-  id: string;
-  title: string;
-  price: string;
-};
-const columns: ColumnsType<AddOnDataType> = [
-  {
-    title: "Add-On Name",
-    dataIndex: "title",
-  },
-  {
-    title: "Price",
-    dataIndex: "price",
-    width: "100px",
-  },
-];
-
-const addOnData: AddOnDataType[] = [
-  {
-    title: "Add-On 1",
-    price: "100",
-    id: "1",
-  },
-  {
-    title: "Add-On 2",
-    price: "200",
-    id: "2",
-  },
-  {
-    id: "3",
-    title: "Add-On 3",
-    price: "300",
-  },
-  {
-    id: "4",
-    title: "Add-On 4",
-    price: "400",
-  },
-  {
-    id: "5",
-    title: "Add-On 5",
-    price: "500",
-  },
-  {
-    id: "6",
-    title: "Add-On 6",
-    price: "600",
-  },
-  {
-    id: "7",
-    title: "Add-On 7",
-    price: "700",
-  },
-];
 
 const AddOnFormSection: React.FC<AddOnFormSectionProps> = () => {
   return (
