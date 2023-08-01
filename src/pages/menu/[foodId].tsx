18a4a0d1b32c69a9f56ebfe95a7630bfeb5e4095
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-08-01 13:37:35 +0700

    Add back button

--- src/pages/menu/[foodId].tsx
+++ src/pages/menu/[foodId].tsx
@@ -3,6 +3,8 @@ import Content from "@/modules/user/food/components/Content";
 import SaveButton from "@/modules/user/food/components/SaveButton";
 import { foods } from "@/modules/user/mock/foods";
 import styled from "@emotion/styled";
+import { ArrowLeft } from "@phosphor-icons/react";
+import { Button } from "antd";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -17,18 +19,31 @@ const FoodDetail = () => {
   return (
     <>
       <LayoutContainer>
-        <Image
-          src={food?.imagePath ?? ""}
-          alt={food?.name ?? ""}
-          width={1000}
-          height={1000}
-          style={{
-            objectFit: "cover",
-            objectPosition: "center",
-            width: "100%",
-            height: "264px",
-          }}
-        />
+        <ImageContainer>
+          <Image
+            src={food?.imagePath ?? ""}
+            alt={food?.name ?? ""}
+            width={1000}
+            height={1000}
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+              width: "100%",
+              height: "264px",
+              position: "absolute",
+            }}
+          />
+          <BackButton
+            shape="circle"
+            size="large"
+            icon={<ArrowLeft size={16} />}
+            style={{
+              position: "absolute",
+              top: "24px",
+              left: "24px",
+            }}
+          />
+        </ImageContainer>
         <Content food={food} />
         <AddToCardButtonNav>
           <AddMinusButton count={dishCount} setCount={setDishCount} />
@@ -61,4 +76,12 @@ const AddToCardButtonNav = styled.div`
   justify-content: space-between;
   padding: 20px 32px;
   align-items: center;
+`;
+
+const BackButton = styled(Button)``;
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 264px;
 `;
