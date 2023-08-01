a9ef12aedb190dc7f333fc41c5af44579edc18e9
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-08-01 14:49:40 +0700

    Change addons to addon

--- src/modules/user/food/components/AddonsCard.tsx
+++ src/modules/user/food/components/AddonsCard.tsx
@@ -5,21 +5,21 @@ import { Checkbox } from "antd";
 import { type CheckboxChangeEvent } from "antd/es/checkbox";
 
 export type AddonsCardProps = {
-  addons: AddOnDataType;
+  addon: AddOnDataType;
 };
 
 const handleChange = (e: CheckboxChangeEvent, addonTitle: string) => {
   console.log("Addon:", addonTitle, "checked", e.target.checked);
 };
 
-const AddonsCard: React.FC<AddonsCardProps> = ({ addons }) => {
+const AddonsCard: React.FC<AddonsCardProps> = ({ addon }) => {
   return (
     <div style={{ marginInline: "12px" }}>
       <FlexRow>
-        <Checkbox onChange={(e) => handleChange(e, addons.title)}>
-          {addons.title}
+        <Checkbox onChange={(e) => handleChange(e, addon.title)}>
+          {addon.title}
         </Checkbox>
-        <Text type="secondary">+{addons.price} Baht</Text>
+        <Text type="secondary">+{addon.price} Baht</Text>
       </FlexRow>
     </div>
   );
