6dea9bd5bf6c980394a90b39741f519c45cd7b8b
Katid Supawee <katidd17@gmail.com>
2023-09-06 14:46:09 +0700

    Move file: merge login&register

---
+++ src/modules/common/hooks/useAuth.ts
@@ -0,0 +1,41 @@
+import { useUserStore } from "@/modules/common/hooks/useUserStore";
+import {
+  getUser,
+  login,
+  register,
+  type LoginBodyParam,
+} from "@/modules/services/auth";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export const useLogin = () => {
+  const { refetch } = useUser();
+  return useMutation({
+    mutationFn: ({ username, password }: LoginBodyParam) =>
+      login({ username, password }),
+    onSuccess: () => {
+      void refetch();
+    },
+  });
+};
+
+export const useRegister = () => {
+  return useMutation({
+    mutationFn: ({ username, password }: LoginBodyParam) =>
+      register({ username, password }),
+  });
+};
+
+export const useUser = () => {
+  const setUser = useUserStore((state) => state.setUser);
+  return useQuery({
+    queryKey: ["user"],
+    retry: 1,
+    queryFn: () => getUser(),
+    onSuccess: (data) => {
+      setUser(data);
+    },
+    onError: () => {
+      setUser(null);
+    },
+  });
+};
--- src/modules/common/hooks/useLogin.ts
+++
@@ -1,9 +0,0 @@
-import { login, type LoginBodyParam } from "@/modules/services/auth";
-import { useMutation } from "@tanstack/react-query";
-
-export const useLogin = () => {
-  return useMutation({
-    mutationFn: ({ username, password }: LoginBodyParam) =>
-      login({ username, password }),
-  });
-};
