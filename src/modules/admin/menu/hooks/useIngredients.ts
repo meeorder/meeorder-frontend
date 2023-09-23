66677d105924e7050800296e5e73031021f0b6d0
GolfGrab <surapus01@gmail.com>
2023-09-23 19:38:09 +0700

    add Ingredient hooks

---
+++ src/modules/admin/menu/hooks/useIngredients.ts
@@ -0,0 +1,45 @@
+import {
+  createIngredient,
+  deleteIngredientById,
+  getAllIngredients,
+  updateIngredientById,
+  type GetAllIngredientsResponse,
+} from "@/modules/services/ingredients";
+import { queryClient } from "@/pages/_app";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export type Ingredient = GetAllIngredientsResponse[number];
+
+export const useCreateIngredient = () => {
+  return useMutation({
+    mutationFn: createIngredient,
+    onSuccess: () => {
+      void queryClient.invalidateQueries(["useAllIngredients"]);
+    },
+  });
+};
+
+export const useAllIngredients = () => {
+  return useQuery({
+    queryKey: ["useAllIngredients"],
+    queryFn: () => getAllIngredients(),
+  });
+};
+
+export const useUpdateIngredient = () => {
+  return useMutation({
+    mutationFn: updateIngredientById,
+    onSuccess: () => {
+      void queryClient.invalidateQueries(["useAllIngredients"]);
+    },
+  });
+};
+
+export const useDeleteIngredient = () => {
+  return useMutation({
+    mutationFn: deleteIngredientById,
+    onSuccess: () => {
+      void queryClient.invalidateQueries(["useAllIngredients"]);
+    },
+  });
+};
