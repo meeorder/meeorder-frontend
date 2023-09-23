c5af00cc33070ecebdcbe6930b636e11abfd7414
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-09-23 23:43:44 +0700

    page preparing

---
+++ src/pages/admin/setting/user-management.tsx
@@ -0,0 +1,11 @@
+import AppLayout from "@/modules/AppLayout";
+
+const AdminUserManagement = () => {
+  return (
+    <AppLayout layoutType="admin" currentPageId="adminUserManagement">
+      adminUserManagement
+    </AppLayout>
+  );
+};
+
+export default AdminUserManagement;
