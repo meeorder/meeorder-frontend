4c0171afa1d69b99ee24b03e4753fad192195ca8
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-09-28 19:36:54 +0700

    Add page *account page *edit username page *edit password page Edit userAvartar *add size, fontsize

---
+++ src/pages/account/edit-username.tsx
@@ -0,0 +1,152 @@
+import { H4, H5, Text } from "@/modules/common/components/Typography";
+import { useUser } from "@/modules/common/hooks/useUserStore";
+import styled from "@emotion/styled";
+import { CaretLeft } from "@phosphor-icons/react";
+import { Button, Input } from "antd";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+const EditUsername = () => {
+  const { data: user } = useUser();
+  const router = useRouter();
+
+  const onClickBackButton = () => {
+    void router.back();
+  };
+
+  return (
+    <>
+      <Head>
+        <title>MeeOrder | EditUsername</title>
+        <meta name="description" content="MeeOrder Application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ScreenContainer>
+        <Container>
+          <BackButtonContainer onClick={() => onClickBackButton()}>
+            <CaretLeft size={32} />
+            <Text>กลับสู่หน้าโพรไฟล์</Text>
+          </BackButtonContainer>
+          <ProfileContainer>
+            <H4>แก้ไขชื่อผู้ใช้</H4>
+            <Text type="secondary">ป้อนชื่อผู้ใช้ใหม่และรหัสผ่านของคุณ</Text>
+            <FieldContainer>
+              <UsernameContainer>
+                <H5
+                  style={{
+                    marginBottom: "8px",
+                  }}
+                >
+                  ชื่อผู้ใช้
+                </H5>
+                <Input
+                  style={{
+                    marginBottom: "8px",
+                    width: "100%",
+                  }}
+                  placeholder="example"
+                ></Input>
+              </UsernameContainer>
+              <PasswordContainer>
+                <H5
+                  style={{
+                    marginBottom: "8px",
+                  }}
+                >
+                  รหัสผ่านปัจจุบัน
+                </H5>
+                <Input
+                  style={{
+                    marginBottom: "8px",
+                    width: "100%",
+                  }}
+                  placeholder="example"
+                ></Input>
+              </PasswordContainer>
+            </FieldContainer>
+            <ChangeUsernameButton>
+              <Text style={{ color: "#fff" }}>เปลี่ยนชื่อผู้ใช้</Text>
+            </ChangeUsernameButton>
+          </ProfileContainer>
+        </Container>
+      </ScreenContainer>
+    </>
+  );
+};
+
+export default EditUsername;
+
+const ScreenContainer = styled.div`
+  min-height: 100dvh;
+  max-width: 500px;
+  margin: 0 auto;
+  background-color: #fafafa;
+`;
+
+const Container = styled.div`
+  min-height: 100dvh;
+  height: 100%;
+  width: 100%;
+  padding-top: 16px;
+  padding-inline: 20px;
+  background: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0) 72.33%,
+      ${(props) => props.theme.antd.colorPrimaryBorder} 115.11%
+    ),
+    linear-gradient(
+      180deg,
+      ${(props) => props.theme.antd.colorPrimaryBorder} -27.67%,
+      #fff 18.01%
+    );
+`;
+
+const BackButtonContainer = styled(Button)`
+  display: flex;
+  justify-content: space-between;
+  width: 30%;
+  align-items: center;
+  min-width: 175px;
+  margin-bottom: 12px;
+  border: none;
+  box-shadow: none;
+  background: none;
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  padding: 8px 0px;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 8px;
+  background: #fff;
+`;
+
+const UsernameContainer = styled.div`
+  margin-bottom: 12px;
+  width: 100%;
+`;
+
+const PasswordContainer = styled.div`
+  margin-bottom: 12px;
+  width: 100%;
+`;
+
+const FieldContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 8px;
+`;
+
+const ChangeUsernameButton = styled(Button)`
+  width: 120px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 2px;
+  background-color: ${(props) => props.theme.antd.colorPrimary};
+  border-radius: 1px solid ${(props) => props.theme.antd.colorPrimary};
+`;
