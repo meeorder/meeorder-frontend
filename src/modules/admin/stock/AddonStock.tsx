71382b2367b492302e8b053e9cf7f1fd72aedb61
AdroitBit <yanothaic15@gmail.com>
2023-09-30 00:33:42 +0700

    do as reviewed 2

--- src/modules/admin/stock/AddonStock.tsx
+++ src/modules/admin/stock/AddonStock.tsx
@@ -10,7 +10,6 @@ import { Card, Switch, Table } from "antd";
 import { type ColumnsType } from "antd/es/table";
 
 const AddonStock = () => {
-  // const [dataSource, setDataSource] = useState(stockAddonData);
   const { data: dataSource } = useAllAddons();
   const { mutate: changeAddonStatus } = useChangeAddonStatus();
   const { mutate: activateAllAddons } = useActivateAllAddons();
