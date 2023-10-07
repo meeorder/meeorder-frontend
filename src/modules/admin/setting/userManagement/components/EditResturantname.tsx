66f5f51fedb1c4b0d9285984843e5f7d1272654f
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-10-08 01:16:55 +0700

    Add edit resturant name

---
+++ src/modules/admin/setting/userManagement/components/EditResturantname.tsx
@@ -0,0 +1,162 @@
+import { H5, Text } from "@/modules/common/components/Typography";
+import styled from "@emotion/styled";
+import { Button, Form, Input, notification } from "antd";
+import React from "react";
+
+type FieldType = {
+  resturantName: string;
+};
+
+type Props = {
+  activeKeys: string[];
+  setActiveKeys: (activeKeys: string[]) => void;
+};
+
+const UpdateActiveKeys = (
+  activeKeys: string[],
+  setActiveKeys: (activeKeys: string[]) => void,
+) => {
+  const newActiveKeys = activeKeys.filter((key) => key !== key);
+  setActiveKeys(newActiveKeys);
+};
+
+const EditResturantname: React.FC<Props> = ({ activeKeys, setActiveKeys }) => {
+  const [form] = Form.useForm<FieldType>();
+  //   const {
+  //     mutate: editResturantName,
+  //     isSuccess,
+  //     isError,
+  //     error,
+  //   } = useUpdateResturant({
+  //     OnSuccess: () => UpdateActiveKeys(activeKeys, setActiveKeys),
+  //   });
+  const [api, contextHolder] = notification.useNotification();
+
+  const handleCancelForm = () => {
+    form.resetFields();
+    UpdateActiveKeys(activeKeys, setActiveKeys);
+  };
+
+  const handleEditResturantName = (values: FieldType) => {
+    const { resturantName: name } = values;
+    // editUsername({
+    //   newUsername: username,
+    //   oldPassword: password,
+    // });
+
+    // editResturantName({
+    //   newName: name,
+    // });
+  };
+
+  //   useEffect(() => {
+  //     if (isError) {
+  //       const axiosErrorMessage =
+  //         (
+  //           error as AxiosError<{
+  //             message: string;
+  //           }>
+  //         )?.response?.data?.message || " ";
+
+  //       form.setFields([
+  //         {
+  //           name: "username",
+  //           errors: [""],
+  //         },
+  //         {
+  //           name: "password",
+  //           errors: [axiosErrorMessage],
+  //         },
+  //       ]);
+  //     }
+  //     if (isSuccess) {
+  //       form.resetFields();
+  //     }
+  //   }, [isError, form, error, isSuccess]);
+
+  //   useEffect(() => {
+  //     const openNotification = (
+  //       placement: NotificationPlacement,
+  //       header: React.ReactNode,
+  //       desciption: React.ReactNode,
+  //       icon?: React.ReactNode,
+  //       onClose?: () => void,
+  //     ) => {
+  //       api.destroy();
+  //       api.info({
+  //         message: header,
+  //         placement,
+  //         description: desciption,
+  //         icon: icon,
+  //         onClose: onClose,
+  //       });
+  //     };
+
+  //     if (isSuccess) {
+  //       openNotification(
+  //         "topRight",
+  //         <H3 style={{ marginLeft: "4px" }}>สำเร็จ</H3>,
+  //         <Text style={{ marginLeft: "4px" }}>แก้ไขชื่อผู้ใช้สำเร็จ</Text>,
+  //         <CheckCircle size={32} color="#A0D911" weight="fill" />,
+  //       );
+  //     }
+  //     if (isError) {
+  //       openNotification(
+  //         "topRight",
+  //         <H3 style={{ marginLeft: "4px" }}>ไม่สำเร็จ</H3>,
+  //         <Text style={{ marginLeft: "4px" }}>แก้ไขชื่อผู้ใช้ไม่สำเร็จ</Text>,
+  //         <XCircle size={32} color="#F5222D" weight="fill" />,
+  //       );
+  //     }
+  //   }, [isSuccess, isError, api, router, setActiveKeys]);
+
+  return (
+    <>
+      {contextHolder}
+      <Container>
+        <Form<FieldType> form={form} onFinish={handleEditResturantName}>
+          <div>
+            <H5 style={{ textAlign: "center" }}>เปลี่ยนชื่อร้านอาหาร</H5>
+            <div style={{ textAlign: "center", width: "100%" }}>
+              <Text type="secondary">
+                ป้อนชื่อผู้ร้านอาหารใหม่และรหัสผ่านของคุณ
+              </Text>
+            </div>
+          </div>
+          <div>
+            <Text>ชื่อร้านอาหาร</Text>
+            <Form.Item<FieldType>
+              name="resturantName"
+              rules={[{ required: true, message: "กรุณากรอกชื่อร้านอาหาร" }]}
+            >
+              <Input />
+            </Form.Item>
+          </div>
+          <ButtonContainer>
+            <Button onClick={handleCancelForm} type="text">
+              ยกเลิก
+            </Button>
+            <Form.Item<FieldType>>
+              <Button htmlType="submit" type="primary">
+                เสร็จสิ้น
+              </Button>
+            </Form.Item>
+          </ButtonContainer>
+        </Form>
+      </Container>
+    </>
+  );
+};
+
+export default EditResturantname;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
---
+++ src/pages/admin/setting/resturant-account-management.tsx
@@ -0,0 +1,118 @@
+import AppLayout from "@/modules/AppLayout";
+import EditResturantname from "@/modules/admin/setting/userManagement/components/EditResturantname";
+import { H3, H4, H5, Text } from "@/modules/common/components/Typography";
+import styled from "@emotion/styled";
+import { PencilSimpleLine } from "@phosphor-icons/react";
+import { Collapse, type CollapseProps } from "antd";
+import { useState } from "react";
+
+const AdminResturantAccountManagement = () => {
+  const [activeKeys, setActiveKeys] = useState<string[]>([]);
+
+  const EditName: CollapseProps["items"] = [
+    {
+      key: "1",
+      label: (
+        <H5 bold style={{ marginLeft: "16px" }}>
+          ชื่อร้านอาหาร
+        </H5>
+      ),
+      children: (
+        <EditResturantname
+          activeKeys={activeKeys}
+          setActiveKeys={setActiveKeys}
+        />
+      ),
+      //   extra: user?.username ?? "ชื่อผู้ใช้งาน",
+      extra: "ชื่อร้านอาหาร",
+    },
+  ];
+
+  return (
+    <AppLayout
+      layoutType="admin"
+      currentPageId="adminResturantAccountManagement"
+    >
+      <MainContainer>
+        <H3>จัดการโลโก้และร้านอาหาร</H3>
+        <EditLogoContainer>
+          <H4
+            style={{
+              marginBottom: "16px",
+            }}
+          >
+            โลโก้ร้านอาหาร
+          </H4>
+          <Logo>
+            <EditIcon>
+              <PencilSimpleLine size={24} color="white" />
+            </EditIcon>
+          </Logo>
+        </EditLogoContainer>
+        <EditRestaurantContainer>
+          <H4
+            style={{
+              marginBottom: "16px",
+            }}
+          >
+            ข้อมูลร้านอาหาร
+          </H4>
+          <Collapse
+            accordion
+            items={EditName}
+            onChange={(key) => {
+              setActiveKeys(key as string[]);
+            }}
+            activeKey={activeKeys}
+            collapsible="icon"
+            expandIcon={() => <Text style={{ color: "#1890FF" }}>แก้ไข</Text>}
+            expandIconPosition="end"
+          />
+        </EditRestaurantContainer>
+      </MainContainer>
+    </AppLayout>
+  );
+};
+
+export default AdminResturantAccountManagement;
+
+const MainContainer = styled.div`
+  display: flex;
+  gap: 24px;
+  flex-direction: column;
+  padding: 24px;
+  width: 100%;
+  background-color: white;
+  border-radius: 8px;
+`;
+
+const EditLogoContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
+const Logo = styled.div`
+  position: relative;
+  width: 200px;
+  height: 200px;
+
+  border-radius: 200px;
+  background:
+    url(https://picsum.photos/200/306),
+    lightgray 0px 0px / 100% 100% no-repeat;
+`;
+
+const EditIcon = styled.div`
+  position: absolute;
+  top: 150px;
+  left: 150px;
+
+  padding: 8px;
+  border-radius: 100px;
+  border: 1px solid var(--primary-6, #1890ff);
+  background: var(--primary-6, #1890ff);
+  box-shadow: 0px 2px 0px 0px rgba(0, 0, 0, 0.04);
+`;
+
+const EditRestaurantContainer = styled.div``;
