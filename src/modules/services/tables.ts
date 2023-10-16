1cc36fd2977ade5e540767e0cd63eac90786fe13
Katid Supawee <katidd17@gmail.com>
2023-10-16 15:28:49 +0700

    Add updated api service& schema

--- src/modules/services/tables.ts
+++ src/modules/services/tables.ts
@@ -25,3 +25,47 @@ export const getAllTables = async (): Promise<GetAllTablesResponse> => {
   const { data } = await axiosInstance.get<GetAllTablesResponse>("/tables");
   return data;
 };
+
+//================>>>> Get a table by id <<<<=========================================//
+export type GetTableByIdResponse =
+  paths["/tables/{id}"]["get"]["responses"]["200"]["content"]["application/json"];
+
+export type GetTableByIdParams =
+  paths["/tables/{id}"]["get"]["parameters"]["path"];
+
+export const getTableById = async (params: GetTableByIdParams) => {
+  const { data } = await axiosInstance.get<GetTableByIdResponse>(
+    `/tables/${params.id}`,
+  );
+  return data;
+};
+
+//================>>>> Update a table by id <<<<=========================================//
+export type UpdateTableByIdResponse =
+  paths["/tables"]["put"]["responses"]["200"]["content"]["application/json"];
+
+export type UpdateTableByIdParams =
+  paths["/tables"]["put"]["requestBody"]["content"]["application/json"] &
+    paths["/tables"]["put"]["parameters"]["path"];
+
+export const updateTableById = async (params: UpdateTableByIdParams) => {
+  const { data } = await axiosInstance.put<UpdateTableByIdResponse>(
+    `/tables/${params.id}`,
+    params,
+  );
+  return data;
+};
+
+//================>>>> Delete a table <<<<=========================================//
+export type DeleteTableByIdResponse =
+  paths["/tables"]["delete"]["responses"]["204"]["content"]["application/json"];
+
+export type DeleteTableByIdParams =
+  paths["/tables"]["delete"]["parameters"]["path"];
+
+export const deleteTableById = async (params: DeleteTableByIdParams) => {
+  const { data } = await axiosInstance.delete<DeleteTableByIdResponse>(
+    `/tables/${params.id}`,
+  );
+  return data;
+};
