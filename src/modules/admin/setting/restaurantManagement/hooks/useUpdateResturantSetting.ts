fa0e4f3510f562a5cf797fe53c982bdee5c8c90a
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-10-28 15:28:15 +0700

    Update can edit coupon ratio

--- src/modules/admin/setting/restaurantManagement/hooks/useUpdateResturantSetting.ts
+++ src/modules/admin/setting/restaurantManagement/hooks/useUpdateResturantSetting.ts
@@ -2,22 +2,17 @@ import { updateSetting } from "@/modules/services/setting";
 import { queryClient } from "@/pages/_app";
 import { useMutation } from "@tanstack/react-query";
 
-type RestaurantSetting = {
-  name?: string;
-  logo?: string;
-};
-
 type RestaurantUpdateParam = {
   onSuccess?: () => void;
 };
 
-const useUpdateRestaurantSetting = (params: RestaurantUpdateParam) => {
+const useUpdateRestaurantSetting = (params?: RestaurantUpdateParam) => {
   return useMutation({
     mutationKey: ["useUpdateRestaurantSetting"],
-    mutationFn: (data: RestaurantSetting) => updateSetting(data),
+    mutationFn: updateSetting,
     onSuccess: () => {
       void queryClient.invalidateQueries(["useRestaurantSetting"]);
-      params.onSuccess && params.onSuccess();
+      params?.onSuccess && params.onSuccess();
     },
   });
 };
