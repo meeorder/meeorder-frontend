48751ffc9dd5d2e5bc85382908e70740c137b28d
GolfGrab <surapus01@gmail.com>
2023-11-05 11:48:14 +0700

    hotfix add search feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import AppLayout from "@/modules/AppLayout";
+import { Text } from "@/modules/common/components/Typography";
 import { useSetSessionUser } from "@/modules/common/hooks/useSetSessionUser";
 import { pages } from "@/modules/pageConfig";
 import Category from "@/modules/user/menu/components/Category";
@@ -6,9 +7,11 @@ import CategoryNav from "@/modules/user/menu/components/CategoryNav";
 import useAllMenu from "@/modules/user/menu/hooks/useAllMenu";
 import { useSessionIdStore } from "@/modules/user/order/hooks/useSession";
 import styled from "@emotion/styled";
+import { MagnifyingGlass, SmileySad } from "@phosphor-icons/react";
+import { Button, Empty, Input } from "antd";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
   const allMenu = useAllMenu();
@@ -31,13 +34,21 @@ const Home = () => {
     setSessionUser();
   }, [setSessionUser]);
 
+  const [searchText, setSearchText] = useState("");
+  const onSearch = (value: string) => setSearchText(value);
+
   const filterMenus = allMenu?.data
     ?.sort((a, b) => (a?.category?.rank ?? 0) - (b?.category?.rank ?? 0))
     ?.map((item) => {
       return {
         ...item,
         menus: item?.menus?.filter(
-          (menu) => menu?.can_order === true && !!menu,
+          (menu) =>
+            menu?.can_order === true &&
+            !!menu &&
+            [...searchText].every(
+              (char) => menu?.title?.toLowerCase().includes(char.toLowerCase()),
+            ),
         ),
       };
     })
@@ -59,15 +70,50 @@ const Home = () => {
           }
         />
         <MenuContainer>
-          {filterMenus?.map((item) => {
-            return (
-              <Category
-                key={item?.category?._id}
-                category={item?.category}
-                menus={item?.menus}
-              />
-            );
-          })}
+          <Input
+            placeholder="ค้นหาเมนู"
+            allowClear
+            onChange={(e) => {
+              onSearch(e.target.value);
+            }}
+            style={{ width: "100%", zIndex: 0 }}
+            suffix={<MagnifyingGlass />}
+          />
+          {filterMenus?.length ? (
+            filterMenus?.map((item) => {
+              return (
+                <Category
+                  key={item?.category?._id}
+                  category={item?.category}
+                  menus={item?.menus}
+                />
+              );
+            })
+          ) : (
+            <Empty
+              description=""
+              image={
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    gap: "20px",
+                  }}
+                >
+                  <SmileySad size={200} color="gray" />
+                  <Text type="secondary">ไม่พบเมนูที่คุณต้องการ</Text>
+                  <Button type="primary" onClick={() => setSearchText("")}>
+                    ล้างการค้นหา
+                  </Button>
+                </div>
+              }
+              style={{
+                marginTop: "20px",
+              }}
+            />
+          )}
         </MenuContainer>
       </AppLayout>
     </>
